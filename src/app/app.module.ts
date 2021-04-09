84e2eb005c5e1a2e144e9a34e48360b89f90150b
NabilAjwad <nrosedi@novatis.com.my>
2021-04-09 09:44:59 +0800

    Updated

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { NgxQRCodeModule } from "ngx-qrcode2";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCovSl4atTfqA6x3CDgn97B8ozr6EoEisc",
+    authDomain: "unipapp.firebaseapp.com",
+    databaseURL: "https://unipapp.firebaseio.com",
+    projectId: "unipapp",
+    storageBucket: "",
+    messagingSenderId: "52047596843",
+    appId: "1:52047596843:web:6e5bc4d4a8bcf213"
+};
+
+@NgModule({
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireAuthModule,
+        NgxQRCodeModule
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
